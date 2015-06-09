7d1103cfb5a13f2fe0bdea8e791aa8bb45f86b89
Tomasz Krzemiński <tomasz.krzeminski@gmail.com>
2015-06-10 00:59:37 +0200

    Added --force to puppet modules install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+# insert valid proxy server, if host is behind some rigorous firewall
 # export {http_proxy,https_proxy,ftp_proxy}='http://10.11.46.234:8118';
 
 [ ! -d /etc/puppet/modules ] && mkdir -p /etc/puppet/modules;
-puppet module install puppetlabs/apache
-puppet module install puppetlabs-java
-puppet module install ssm-munin
-puppet module install ajcrowe-supervisord
+
+# install required puppet modules
+puppet module install puppetlabs/apache --force
+puppet module install puppetlabs-java --force
+puppet module install ssm-munin --force
+puppet module install ajcrowe-supervisord --force
