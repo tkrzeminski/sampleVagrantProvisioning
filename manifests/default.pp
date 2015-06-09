ec5ba38949ed91d735c32758ec277ce26c1f6206
Tomasz Krzemiński <tomasz.krzeminski@gmail.com>
2015-06-10 00:57:23 +0200

    Added supervisor, maven build

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,9 +1,13 @@
 
-# Define tool items 
+# Make sure basic tools are installed
 class base {
 
 	package { 'htop':
 		name => 'htop',
+		ensure => present
+	}
+
+	package { 'maven':
 		ensure => present
 	}
 
@@ -18,6 +22,11 @@ class base {
 }
 
 
+# define path for commands
+Exec {
+	path => [ "/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin" ]
+}
+
 
 #
 # APACHE: define params
@@ -28,7 +37,8 @@ class { 'apache':
 	serveradmin => 'tomasz@krzeminski.info',
 	server_signature => 'Off',
 	server_tokens => 'Prod',
-	docroot => '/var/www/html'
+	docroot => '/var/www/html',
+	default_vhost => false,
 }
 
 class { 'apache::mod::status':
@@ -42,6 +52,17 @@ apache::vhost { '4fit':
 	port => 80,
 	options => ['None'],
 	docroot  => '/var/www/html-4fit',
+	proxy_pass => [
+		{ 'path' => "/ping", 'url' => 'http://localhost:8081/ping' },
+		{ 'path' => "/stress", 'url' => 'http://localhost:8081/stress' },
+	],
+	custom_fragment => 'Alias /munin /var/cache/munin/www',
+	directories => [ {
+			path => '/var/cache/munin/www',
+			options => ['None'],
+			allow_override => [ 'None' ],
+		}
+	]
 }
 
 # and for SSL
@@ -50,12 +71,26 @@ apache::vhost { '4fitssl':
 	options => ['None'],
 	docroot  => '/var/www/html-4fit',
 	ssl => true,
-} -> # after then
+	proxy_pass => [
+		{ 'path' => "/ping", 'url' => 'http://localhost:8081/ping' },
+		{ 'path' => "/stress", 'url' => 'http://localhost:8081/stress' },
+	],
+	custom_fragment => 'Alias /munin /var/cache/munin/www',
+	directories => [ {
+			path => '/var/cache/munin/www',
+			options => ['None'],
+			allow_override => [ 'None' ],
+		}
+	]
+}
+-> # after then
 # Create welcome file for custom vhosts
 file { '/var/www/html-4fit/index.html':
 	ensure => present,
-	content => '<h1>Hello 4FIT!</html>'	
+	content => '<h1>Hello 4FIT!</h1><p>Check out <a href="/munin">monitoring</a> or <a href="/ping">ping</a> simple java server</p>'
 }
+
+
 
 #
 # Monitoring with munin
@@ -65,7 +100,7 @@ munin::master::node_definition { 'localhost':
     config  => [ 'use_node_name yes' ],
 }
 
-# use cron task as default (refresh every 5 min)
+# use cron task as default strategy
 class { 'munin::master' :
 	graph_strategy => 'cron',
 	html_strategy => 'cron',
@@ -74,16 +109,31 @@ class { 'munin::master' :
 
 class { 'munin::node' :
 	allow => [ '127.0.0.1', '192.168.55.10', '::1' ]
-} ->
-file { '/etc/apache2/conf.d/munin.conf' :
-	ensure => link,
-	target => '/etc/munin/apache.conf'
 }
 
 
 
 #
-# Java
+# Supervisord - take care of simple java server
+#
+class { 'supervisord':
+	install_pip => true,
+	require => Exec[ "create package" ],
+	
+}
+
+supervisord::program { 'SimpleServer':
+	command     => 'java -cp server-1.0-SNAPSHOT.jar info.krzeminski.server.SimpleHttpServer',
+	priority    => '100',
+	directory => "/opt/simpleServer/",
+	environment => {
+		'PATH'   => '/bin:/sbin:/usr/bin:/usr/sbin',
+	}
+}
+
+
+#
+# JDK
 #
 
 class { 'java':
@@ -91,11 +141,29 @@ class { 'java':
 }
 
 
+#
+# Build simple http server
+#
+file { "/opt/simpleServer/":
+    ensure => "directory"
+}
+# build simpleServer maven package
+exec { "create package":
+	require => [ Class["java"], Package["maven"], File["/opt/simpleServer"] ],
+	cwd => "/vagrant/SimpleServer/server/",
+	command => "mvn package -Dalt.build.dir=/opt/simpleServer/"
+}
+
+
+
+
+
 # Fire up required modules
 
 include base
 include apache
 include java
+include supervisord
 include 'apache::mod::php'
 include 'apache::mod::info'
 include 'apache::mod::status'
